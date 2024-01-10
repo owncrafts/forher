383f31d440427e0063da12b4dfe26f61b366e305
root <root@GreenCode>
2024-01-10 16:24:38 +0530

    Text Encryption with Password
    Made by Zade Meadows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,199 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import CryptoJS from "crypto-js";
+import ClipboardJS from "clipboard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
+const EncryptDecryptPage = () => {
+  const [inputText, setInputText] = useState("");
+  const [password, setPassword] = useState("");
+  const [encryptedText, setEncryptedText] = useState("");
+  const [decryptedText, setDecryptedText] = useState("");
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [activeTab, setActiveTab] = useState("encrypt");
+
+  const encryptText = () => {
+    const encrypted = CryptoJS.AES.encrypt(inputText, password).toString();
+    setEncryptedText(encrypted);
+    toast.success("Text encrypted successfully!");
+  };
+
+  const decryptText = () => {
+    try {
+      const decrypted = CryptoJS.AES.decrypt(encryptedText, password).toString(
+        CryptoJS.enc.Utf8
+      );
+      setDecryptedText(decrypted);
+      setIsPasswordValid(true);
+      if (decrypted != "" || decrypted != null) {
+        toast.success("Text decrypted successfully!");
+        console.log("'"+decrypted+"'");
+      } else {
+        toast.error("Wrong password! Please try again.");
+      }
+    } catch (error) {
+      setIsPasswordValid(false);
+      toast.error("Wrong password! Please try again.");
+      setTimeout(() => {
+        setIsPasswordValid(true);
+      }, 2000); // Show warning for 2 seconds
+    }
+  };
+const handleCopy = (textToCopy) => {
+  const clipboard = new ClipboardJS(".copy-button", {
+    text: () => textToCopy,
+  });
+
+  clipboard.on("success", (e) => {
+    console.log("Text copied to clipboard:", e.text);
+    clipboard.destroy();
+    toast.success("Text copied to clipboard!");
+  });
+
+  clipboard.on("error", (e) => {
+    console.error("Unable to copy:", e.action);
+    clipboard.destroy();
+    toast.error("Failed to copy text to clipboard.");
+  });
+
+  // Manually trigger the click event on a hidden button with class '.copy-button'
+  const hiddenButton = document.createElement("button");
+  hiddenButton.className = "copy-button";
+  document.body.appendChild(hiddenButton);
+  hiddenButton.click();
+  document.body.removeChild(hiddenButton);
+};
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prevState) => !prevState);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+    <div className="w-full bg-black text-white p-4">
+      <h2 className="text-xl text-center font-semibold">Lock Your Message Here</h2>
+    </div>
+    <div className="p-4">
+      <div className="flex justify-center space-x-4 mb-4">
+        <button
+          className={`py-2 px-4 rounded-md ${
+            activeTab === "encrypt" ? "bg-gray-500 text-white" : "bg-gray-300"
+          }`}
+          onClick={() => setActiveTab("encrypt")}
+        >
+          Encrypt
+        </button>
+        <button
+          className={`py-2 px-4 rounded-md ${
+            activeTab === "decrypt" ? "bg-gray-500 text-white" : "bg-gray-300"
+          }`}
+          onClick={() => setActiveTab("decrypt")}
+        >
+          Decrypt
+        </button>
+      </div>
+      <ToastContainer />
+      {activeTab === "encrypt" && (
+        <div className="flex flex-col items-center">
+          <textarea
+            rows={4}
+            className="border rounded-md p-2 mb-2 w-80"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Enter Message to Lock it"
+          />
+          <div className="relative">
+            <input
+              className="border rounded-md p-2 mb-2 w-80 pe-[40px]"
+              type={isPasswordVisible ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
             />
-          </a>
+            <button
+              className="bg-gray-200 rounded-md p-2 mb-2 absolute right-[5px] top-[10%]"
+              onClick={togglePasswordVisibility}
+            >
+              {isPasswordVisible ? <FiEyeOff /> : <FiEye />}
+            </button>
+          </div>
+          <button
+            className="bg-gray-500 text-white py-2 px-4 rounded-md mb-2 w-80"
+            onClick={encryptText}
+          >
+            Lock Message
+          </button>
+          <textarea
+            className="border rounded-md p-2 w-80 h-24"
+            value={encryptedText}
+            disabled
+          />
+          <button
+            className="bg-gray-500 text-white py-2 px-4 rounded-md my-2 w-80"
+            onClick={() => handleCopy(encryptedText)}
+          >
+            Copy
+          </button>
         </div>
-      </div>
+      )}
+      {activeTab === "decrypt" && (
+        <div className="flex flex-col items-center">
+          <textarea
+            rows={4}
+            className="border rounded-md p-2 mb-2 w-80"
+            value={encryptedText}
+            onChange={(e) => setEncryptedText(e.target.value)}
+            placeholder="Enter Message to Unlock it"
+          />
+          <div className="relative">
+            <input
+              className="border rounded-md p-2 mb-2 w-80 pe-[40px]"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+            />
+            <button
+              className="bg-gray-200 rounded-md p-2 mb-2 absolute right-[5px] top-[10%]"
+              onClick={togglePasswordVisibility}
+            >
+              {isPasswordVisible ? <FiEyeOff /> : <FiEye />}
+            </button>
+          </div>
+          <button
+            className="bg-gray-500 text-white py-2 px-4 rounded-md mb-2 w-80"
+            onClick={decryptText}
+          >
+            Unlock Message
+          </button>
+          {!isPasswordValid && (
+            <div className="text-red-500 mb-2">
+              Wrong password! Please try again.
+            </div>
+          )}
+          <textarea
+            className="border rounded-md p-2 w-80 h-24"
+            value={decryptedText}
+            disabled
+          />
+          <button
+            className="bg-gray-500 text-white py-2 px-4 rounded-md my-2 w-80"
+            onClick={() => handleCopy(decryptedText)}
+          >
+            Copy
+          </button>
+        </div>
+      )}
+    </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div className="my-5">
+      <p className="text-center font-[14px] font-semibold">Made with ❤️ by Zade Meadows</p>
+    </div>
+    </>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default EncryptDecryptPage;
